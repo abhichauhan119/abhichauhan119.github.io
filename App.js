08380c557c907518d4a09ff80aa7acc8f3b89a12
abhichauhan119 <abhichauhan119@gmail.com>
2024-09-19 19:44:41 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,3 +1,27 @@
+const urlB64ToUint8Array = base64String => {
+  const padding = "=".repeat((4 - (base64String.length % 4)) % 4);
+  const base64 = (base64String + padding)
+    .replace(/\-/g, "+")
+    .replace(/_/g, "/");
+  const rawData = atob(base64);
+  const outputArray = new Uint8Array(rawData.length);
+  for (let i = 0; i < rawData.length; ++i) {
+    outputArray[i] = rawData.charCodeAt(i);
+  }
+  return outputArray;
+};
+const saveSubscription = async subscription => {
+  const SERVER_URL = "http://localhost:4000/save-subscription";
+  const response = await fetch(SERVER_URL, {
+    method: "post",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(subscription)
+  });
+  return response.json();
+};
+
 const APP = {
   SW: null,
   init() {
